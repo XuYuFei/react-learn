1d30f1e3af75436e108c7549b572c450e8ccac32
XuYuFei <yuranfeng@126.com>
2020-04-18 22:27:06 +0800

    学习：高级指引/2.代码分割 - 学习中

--- AdvancedGuides/learn/src/App.js
+++ AdvancedGuides/learn/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { add } from './math'
+
+// 2.1 打包
+console.log(add(10, 20))
+
+import('./math').then(math => {
+  console.log(math.add(10, 20))
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <div className="App">React</div>
 }
 
 export default App
