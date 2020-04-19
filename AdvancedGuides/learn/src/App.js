ac576c35bfa11c9b06ccad24ef1c669a62b6f113
XuYuFei <yuranfeng@126.com>
2020-04-19 23:54:55 +0800

    学习：高级指引/2.代码分割.md - 学习中

--- AdvancedGuides/learn/src/App.js
+++ AdvancedGuides/learn/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import { add } from './math'
+import { Suspense } from 'react'
 
 // 2.1 打包
 console.log(add(10, 20))
@@ -9,8 +10,25 @@ import('./math').then(math => {
   console.log(math.add(10, 20))
 })
 
+// React.lazy
+const HelloWorld = React.lazy(() => import('./components/HelloWorld'))
+
+function MyComponent() {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <HelloWorld />
+      </Suspense>
+    </div>
+  )
+}
+
 function App() {
-  return <div className="App">React</div>
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  )
 }
 
 export default App
---
+++ AdvancedGuides/learn/src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../logo.svg'
+
+function HelloWorld() {
+  return (
+    <div>
+      <img src={logo} alt="react" />
+      Hello World!
+    </div>
+  )
+}
+
+export default HelloWorld
