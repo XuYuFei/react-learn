4b071e0fc44a664612cfe7e752921b5ce4843982
XuYuFei <yuranfeng@126.com>
2020-04-20 23:03:50 +0800

    学习：高级指引/2.代码分割.md - 完成

--- AdvancedGuides/learn/src/App.js
+++ AdvancedGuides/learn/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css'
 import { add } from './math'
-import { Suspense } from 'react'
+import { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // 2.1 打包
 console.log(add(10, 20))
@@ -31,4 +32,19 @@ function App() {
   )
 }
 
-export default App
+// 基于路由的代码分割
+const Home = lazy(() => import('./routes/Home'))
+const About = lazy(() => import('./routes/About'))
+
+const App2 = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Suspense>
+  </Router>
+)
+
+export default App2
