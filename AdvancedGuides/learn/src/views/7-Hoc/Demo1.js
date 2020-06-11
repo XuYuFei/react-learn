cf8d7dc02af4625ec8ad8d159eb51f44012e47d8
XuYuFei <yuranfeng@126.com>
2020-06-11 18:00:17 +0800

    学习中。。。

--- AdvancedGuides/learn/src/views/7-Hoc/Demo1.js
+++ AdvancedGuides/learn/src/views/7-Hoc/Demo1.js
@@ -95,4 +95,102 @@ class App extends React.Component {
   }
 }
 
-export default App
+/**************************************************************
+ **                         改写                             **
+/**************************************************************/
+/**
+ * 组件函数
+ * @param {*} WrappedComponent 传入的组件
+ * @param {*} selectData 数据源回调方法
+ */
+function withSubscription(WrappedComponent, selectData) {
+  // 返回另一个组件
+  return class extends React.Component {
+    constructor(props) {
+      super(props)
+
+      this.handleChange = this.handleChange.bind(this)
+      this.state = {
+        data: selectData(DataSource, props)
+      }
+    }
+
+    componentDidMount() {
+      // ...负责订阅相关操作...
+      DataSource.addChangeListener(this.handleChange)
+    }
+
+    componentWillUnmount() {
+      DataSource.removeChangeListener(this.handleChange)
+    }
+
+    handleChange() {
+      this.setState({
+        data: selectData(DataSource, this.props)
+      })
+    }
+
+    render() {
+      // ...并使用新数据渲染被包装的组件
+      return <WrappedComponent data={this.state.data} {...this.props} />
+    }
+  }
+}
+
+// 简化后评论组件
+class CommentList2 extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      comments: props.data
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.comments.map(comment => (
+          <Comment comment={comment} key={comment.id} />
+        ))}
+      </div>
+    )
+  }
+}
+
+// 简化后博客组件
+class BlogPost2 extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      blogPost: props.data
+    }
+  }
+
+  render() {
+    return <TextBlock text={this.state.blogPost} />
+  }
+}
+
+const CommentListWithSubscription = withSubscription(CommentList2, DataSource =>
+  DataSource.getComments()
+)
+
+const BlogPostWithSubscription = withSubscription(
+  BlogPost2,
+  (DataSource, props) => DataSource.getBlogPost(props.id)
+)
+
+class App2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <CommentListWithSubscription />
+        <BlogPostWithSubscription id="100" />
+      </div>
+    )
+  }
+}
+
+export default App2
